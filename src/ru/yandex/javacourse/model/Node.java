affea54a17889028e7f8f601c559c4f885dae2aa
Andrey A. Belokur <abelokur@yandex.ru>
2025-07-18 13:06:36 +0300

    Исправил замечания CheckStyle

--- src/ru/yandex/javacourse/model/Node.java
+++ src/ru/yandex/javacourse/model/Node.java
@@ -1,11 +1,11 @@
 package ru.yandex.javacourse.model;
 
-public class Node <E>{
+public class Node<E> {
     public Task data;
     public Node<E> next;
     public Node<E> prev;
 
-    public Node (Node<E> prev, Task data, Node<E> next){
+    public Node ( Node<E> prev, Task data, Node<E> next ){
         this.prev = prev;
         this.data = data;
         this.next = next;
