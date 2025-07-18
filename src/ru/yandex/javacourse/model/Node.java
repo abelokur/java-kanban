230784aa42be4314ab5cf300f1cf773fc21950c0
Andrey A. Belokur <abelokur@yandex.ru>
2025-07-18 13:19:27 +0300

    Исправил доп. замечания CheckStyle

--- src/ru/yandex/javacourse/model/Node.java
+++ src/ru/yandex/javacourse/model/Node.java
@@ -5,7 +5,7 @@ public class Node<E> {
     public Node<E> next;
     public Node<E> prev;
 
-    public Node(Node<E> prev, Task data, Node<E> next){
+    public Node(Node<E> prev, Task data, Node<E> next) {
         this.prev = prev;
         this.data = data;
         this.next = next;
