2b3b9cab7c0366dc2f8ea8feb2219139c67788c1
Andrey A. Belokur <abelokur@yandex.ru>
2025-07-18 13:22:37 +0300

    Исправил доп. замечания CheckStyle

--- src/ru/yandex/javacourse/model/HistoryManager.java
+++ src/ru/yandex/javacourse/model/HistoryManager.java
@@ -3,7 +3,10 @@ package ru.yandex.javacourse.model;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getDefaultHistory();
 }
