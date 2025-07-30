019818df136d78110de5dae55eeddd41090dedf3
Andrey A. Belokur <abelokur@yandex.ru>
2025-07-30 22:10:57 +0300

    Закончил разработку. Комментарии не удалены

--- src/ru/yandex/javacourse/service/FileBackedTaskManager.java
+++ src/ru/yandex/javacourse/service/FileBackedTaskManager.java
@@ -1,19 +1,159 @@
 package ru.yandex.javacourse.service;
 
-import ru.yandex.javacourse.model.Epic;
-import ru.yandex.javacourse.model.Subtask;
-import ru.yandex.javacourse.model.Task;
+import ru.yandex.javacourse.model.*;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
     File fileTaskManager;
+
     public FileBackedTaskManager(File fileTaskManager) {
         this.fileTaskManager = fileTaskManager;
     }
-    private void save() {
 
+    public static FileBackedTaskManager loadFromFile(File file) {
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file);
+        try (FileReader fileReader = new FileReader(fileBackedTaskManager.fileTaskManager.getName());
+             BufferedReader br = new BufferedReader(fileReader)) {
+           /*System.out.println("вычитываем файл:");
+            while (br.ready()) {
+                String line = br.readLine();
+                System.out.println(line);
+            }*/
+            String[] lineTask = new String[6];
+            //String[]
+           // String line =
+                    br.readLine();
+            while (br.ready()) {
+
+
+                String line = br.readLine();
+                String[] lineArray = line.split(",");
+
+                System.out.println(lineArray[0]);
+                System.out.println(lineArray[1]);
+                System.out.println(lineArray[2]);
+                System.out.println(lineArray[3]);
+                System.out.println(lineArray[4]);
+                try {
+                    System.out.println(lineArray[5]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    ;
+                }
+
+                switch (TypeTask.valueOf(lineArray[1])) {
+                    case TypeTask.TASK:
+
+                        Task task = new Task(lineArray[2], lineArray[4]);
+
+                        task.setId(Integer.parseInt(lineArray[0]));
+                        task.setStatus(Status.valueOf(lineArray[3]));
+
+                        fileBackedTaskManager.createTasks(task);
+
+                        break;
+
+                    case TypeTask.SUBTASK:
+
+
+                        Subtask subtask = new Subtask(lineArray[2], lineArray[4]);
+
+                        subtask.setId(Integer.parseInt(lineArray[0]));
+                        subtask.setStatus(Status.valueOf(lineArray[3]));
+                        subtask.setEpic(fileBackedTaskManager.getEpic(Integer.parseInt(lineArray[5])));
+
+                        fileBackedTaskManager.createSubtasks(subtask);
+
+                        break;
+
+                    case TypeTask.EPIC:
+
+                        Epic epic = new Epic(lineArray[2], lineArray[4]);
+
+                        epic.setId(Integer.parseInt(lineArray[0]));
+                        epic.setStatus(Status.valueOf(lineArray[3]));
+
+                        fileBackedTaskManager.createEpics(epic);
+
+                        break;
+                }
+
+                for (String str : lineArray) {
+                    System.out.println(str);
+                    //if ()
+                }
+                /*System.out.println(lineArray[0]);
+                System.out.println(lineArray[1]);
+                System.out.println(lineArray[2]);
+                System.out.println(lineArray[3]);
+                System.out.println(lineArray[4]);
+                System.out.println(lineArray[5]);*/
+
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileBackedTaskManager;
     }
+
+    private void save() {
+        String head = "id,type,name,status,description,epic\n";
+        HashMap<Integer, Task> allTasks = super.getAllTasks();
+        HashMap<Integer, Subtask> allSubTasks = super.getAllSubTasks();
+        HashMap<Integer, Epic> allEpics = super.getAllEpics();
+
+        //StringBuilder fileContents = new StringBuilder();
+
+        try (Writer fileWriter = new FileWriter(fileTaskManager.getName())) {
+
+            fileWriter.write(head);
+
+            for (Integer eachTask : allTasks.keySet()) {
+                Task task = allTasks.get(eachTask);
+                //fileContents.append(toString(task));
+                fileWriter.write(toString(task));
+            }
+
+            for (Integer eachEpic : allEpics.keySet()) {
+                Epic epic = allEpics.get(eachEpic);
+                fileWriter.write(toString(epic));
+            }
+
+            for (Integer eachSubTask : allSubTasks.keySet()) {
+                Subtask subTask = allSubTasks.get(eachSubTask);
+                fileWriter.write(toString(subTask));
+            }
+
+    }catch (IOException e) {
+            e.printStackTrace();
+        }
+/*
+        try (Writer fileWriter = new FileWriter(fileTaskManager.getName())) {
+            fileWriter.write(fileContents.toString());
+            //Writer fileWriter1 = fileTaskManager.;
+        }
+            catch (IOException e) {
+                e.printStackTrace();
+            }*/
+    }
+
+    String toString(Task task) { //TypeTask.valueOf(task.getClass().toString())
+        return task.getId() + "," + TypeTask.TASK + "," + task.getName() + "," + task.getStatus() + "," + task.getDescription() + ",\n";
+    }
+
+    String toString(Subtask subtask) {
+        return subtask.getId() + "," + TypeTask.SUBTASK + "," + subtask.getName() + "," + subtask.getStatus() + "," + subtask.getDescription() + "," + subtask.getEpic().getId() + "\n";
+    }
+
+    String toString(Epic epic) {
+        return epic.getId() + "," + TypeTask.EPIC + "," + epic.getName() + "," + epic.getStatus() + "," + epic.getDescription() + ",\n";
+    }
+
+
 
     @Override
     public void removeTasks() {
